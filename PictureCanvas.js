bebeae972919401ff93201ee611e0026abdc4c9c
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-07-18 11:14:44 -0700

    Add PictureCanval class

---
+++ PictureCanvas.js
@@ -0,0 +1,16 @@
+const scale = 10;
+
+class PictureCanvas{
+    constructor(picture, pointerDown){
+        this.dom = elt('canvas', {
+            onmousedown: event => this.mouse(event, pointerDown),
+            ontouchstart: event => this.touch(event, pointerDown)
+        });
+        this.syncState(picture);
+    }
+    syncState(picture){
+        if(this.picture === picture) return;
+        this.picture = picture;
+        drawPicture(this.picture, this.dom, scale)
+    }
+}
